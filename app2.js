8642c50f1ab2d539a29bf115765a2104f936f1d9
Saviourise <65181992+Saviourise@users.noreply.github.com>
2021-11-24 21:09:25 +0100

    Update app2.js

--- app2.js
+++ app2.js
@@ -17,11 +17,11 @@ if (screen <= 800) {
     exp.innerHTML = "<p onclick='openMenu()'><i class='fa fa-bars'></i></p>";
 
     function openMenu() {
-        nav.innerHTML = "<p class='x' id='x' onclick='closeMenu()'>X</p><p class='home'>Home</p><p class='catalog'>Catalog</p><p class='blog'>Blog</p><p class='about'>About</p><p class='contact'>Contact</p>"
+        exp.innerHTML = "<p class='x' id='x' onclick='closeMenu()'>X</p><p class='home'>Home</p><p class='catalog'>Catalog</p><p class='blog'>Blog</p><p class='about'>About</p><p class='contact'>Contact</p>"
     }
 
     function closeMenu() {
-        nav.innerHTML = "<p onclick='openMenu()'><i class='fa fa-bars'></i></p>";
+        exp.innerHTML = "<p onclick='openMenu()'><i class='fa fa-bars'></i></p>";
     }
 
 
